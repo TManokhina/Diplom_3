ae380e903a8e4e67f92af89adac9f8fd1758d8f6
TManokhina <tmanokhi@yandex.ru>
2024-11-25 11:48:18 +0300

    Create RedirectToLoginPageTest

---
+++ src/test/java/com/example/diplom_3/RedirectToLoginPageTest.java
@@ -0,0 +1,66 @@
+package com.example.diplom_3;
+
+import com.example.diplom_3.page.object.ForgotPasswordPage;
+import com.example.diplom_3.page.object.HomePage;
+import com.example.diplom_3.page.object.LoginPage;
+import com.example.diplom_3.page.object.RegisterPage;
+import io.qameta.allure.Description;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.example.diplom_3.page.object.ForgotPasswordPage.FORGOT_PASSWORD_PATH;
+import static com.example.diplom_3.page.object.HomePage.BASE_URI_PATH;
+import static com.example.diplom_3.page.object.LoginPage.LOGIN_URL;
+import static com.example.diplom_3.page.object.RegisterPage.REGISTER_PATH;
+import static org.junit.Assert.assertEquals;
+
+public class RedirectToLoginPageTest extends BaseSetUpTest {
+
+    protected LoginPage loginPage;
+    protected HomePage homePage;
+    protected RegisterPage registerPage;
+    protected ForgotPasswordPage forgotPasswordPage;
+    @Before
+    public void setUpRedirect() throws Exception {
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    @Description("Проверка перехода по кнопке «Войти в аккаунт» на главной странице к странице логина")
+    public void checkRedirectFromHomePageLogin() {
+        driver.get(BASE_URI_PATH);
+        homePage = new HomePage(driver);
+        homePage.clickOnLoginButton();
+        loginPage.waitForLoad();
+        assertEquals("Не получилось перейти на страницу логина по кнопке «Войти в аккаунт» на главной странице",LOGIN_URL, driver.getCurrentUrl());
+    }
+    @Test
+    @Description("Проверка перехода по кнопке «Войти в аккаунт» на главной странице к странице логина")
+    public void checkRedirectFromHomePagePersonalAccount() {
+        driver.get(BASE_URI_PATH);
+        homePage = new HomePage(driver);
+        homePage.clickOnAccountButton();
+        loginPage.waitForLoad();
+        assertEquals("Не получилось перейти на страницу логина по кнопке «Войти в аккаунт» на главной странице",LOGIN_URL, driver.getCurrentUrl());
+    }
+    @Test
+    @Description("Проверка перехода через кнопку в форме регистрации к странице логина")
+    public void checkRedirectFromRegisterPage() {
+        driver.get(REGISTER_PATH);
+        registerPage = new RegisterPage(driver);
+        registerPage.clickOnLoginButton();
+        loginPage.waitForLoad();
+        assertEquals("Не получилось перейти на страницу логина через кнопку в форме регистрации",LOGIN_URL, driver.getCurrentUrl());
+    }
+
+    @Test
+    @Description("Проверка перехода по кнопке из формы восстановления пароля")
+    public void checkRedirectFromForgotPasswordPage() {
+        driver.get(FORGOT_PASSWORD_PATH);
+        forgotPasswordPage = new ForgotPasswordPage(driver);
+        forgotPasswordPage.clickOnLoginButton();
+        loginPage.waitForLoad();
+        assertEquals("Не получилось перейти на страницу логина через кнопку в форме восстановления пароля",LOGIN_URL, driver.getCurrentUrl());
+    }
+
+}
